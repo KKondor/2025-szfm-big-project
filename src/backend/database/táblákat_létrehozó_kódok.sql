a1adbab930535f3aba76f7be2ba2521c90643466
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-10-27 16:36:33 +0100

    user tábla létrehozása

--- src/backend/database/táblákat_létrehozó_kódok.sql
+++ src/backend/database/táblákat_létrehozó_kódok.sql
@@ -1,1 +1,10 @@
+USE `fast-food_database`;
 
+-- user tábla létrehozása
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  role ENUM('user', 'admin') NOT NULL
+);
