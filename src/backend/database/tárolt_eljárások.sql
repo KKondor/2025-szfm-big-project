1b6ce25a85568d651ccdc86b97b8e203dec68cb3
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-10-31 14:31:26 +0100

    tárolt_eljárások.sql, create_user hozzáadása
    Added a stored procedure to create a new user in the database.

--- src/backend/database/tárolt_eljárások.sql
+++ src/backend/database/tárolt_eljárások.sql
@@ -1,1 +1,16 @@
 USE `fast-food_database`;
+
+-- Új felhasználó létrehozása
+DELIMITER //
+CREATE PROCEDURE create_user(
+    IN p_name VARCHAR(100),
+    IN p_email VARCHAR(100),
+    IN p_password VARCHAR(100),
+    IN p_role ENUM('user', 'admin')
+)
+BEGIN
+    INSERT INTO USERS (name, email, password, role)
+    VALUES (p_name, p_email, p_password, p_role);
+END;
+//
+DELIMITER ;
