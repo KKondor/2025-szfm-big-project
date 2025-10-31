bf11eaed3c3ac326f931e0c5e06bfb1db266e3ff
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-10-31 17:35:12 +0100

    services.py, is_strong_password hozzáadása
    Add function to validate password strength based on criteria.

--- src/backend/services.py
+++ src/backend/services.py
@@ -9,4 +9,12 @@ def is_valid_email(email: str) -> bool:
     pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
     return re.match(pattern, email) is not None
 
-
+#Ellenörzi, hogy a megadott jelszó elég erős e.
+#Megnézi, hogy szerepel e benne: kis betű, nagy betű, szám
+#És megnézi, hogy minimum tartalmaz e 12 karaktert.
+def is_strong_password(password: str) -> bool:
+    has_lower = any(c.islower() for c in password)
+    has_upper = any(c.isupper() for c in password)
+    has_digit = any(c.isdigit() for c in password)
+    long_enough = len(password) >= 12
+    return has_lower and has_upper and has_digit and long_enough
