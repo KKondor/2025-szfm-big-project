d2e70c432f2b3caaa422d9a50ca25a47a4308197
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-10-31 17:39:50 +0100

    services.py, hash_password hozzáadása
    Added a function to hash passwords using bcrypt.

--- src/backend/services.py
+++ src/backend/services.py
@@ -9,6 +9,7 @@ def is_valid_email(email: str) -> bool:
     pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
     return re.match(pattern, email) is not None
 
+
 #Ellenörzi, hogy a megadott jelszó elég erős e.
 #Megnézi, hogy szerepel e benne: kis betű, nagy betű, szám
 #És megnézi, hogy minimum tartalmaz e 12 karaktert.
@@ -18,3 +19,14 @@ def is_strong_password(password: str) -> bool:
     has_digit = any(c.isdigit() for c in password)
     long_enough = len(password) >= 12
     return has_lower and has_upper and has_digit and long_enough
+
+
+#Titkosítja a megadott jelszót bcrypt hash algoritmussal.
+def hash_password(password: str) -> str:
+    try:
+        hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        print("A jelszó titkosítása elkészült.")
+        return hashed
+    except Exception as e:
+        print(f"Hiba történt a jelszó titkosítása során: {e}")
+        return "Hiba a jelszó titkosításakor"
