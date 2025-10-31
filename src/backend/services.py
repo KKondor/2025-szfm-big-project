9329f564e278f93af94d9c1d94b3e0ed6889dbb1
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-10-31 17:29:48 +0100

    services.py, is_valid_email hozzáadása
    Added email validation function to check format.

--- src/backend/services.py
+++ src/backend/services.py
@@ -1,1 +1,12 @@
+import bcrypt
+import re
+from users import UserManager, User
+
+user_manager = UserManager()
+
+#Ellenörni, hogy a megadott email címnek helyes e a formátuma.
+def is_valid_email(email: str) -> bool:
+    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    return re.match(pattern, email) is not None
+
 
