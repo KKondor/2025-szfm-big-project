dbf3300326ac4cbbddec67d7541d2b2876a11564
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-10-31 15:49:47 +0100

    user.py, create_user hozzáadása
    Added UserManager class with create_user method for user creation in the database.

--- src/backend/users.py
+++ src/backend/users.py
@@ -15,3 +15,19 @@ class User:
     email: str
     password: str
     role: Role
+
+class UserManager:
+
+#Új felhasználót hoz létre az adatbázisba
+#Meg kell adani neki bemenetbe:
+#a nevet, az email címet, és a jelszót már titkosított formában
+#Alap értelmezetten user jogot ad a felhasználónak
+    def create_user(self, name: str, email: str, password: str):
+        conn = get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.callproc("create_user", [name, email, password, Role.USER.value])
+            conn.commit()
+        finally:
+            cursor.close()
+            conn.close()
