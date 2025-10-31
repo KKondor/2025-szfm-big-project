f1a739886d3c3714186c3ffe665222f32d96c5c5
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-10-31 15:21:59 +0100

    users.py, User class hozzáadása

--- src/backend/users.py
+++ src/backend/users.py
@@ -1,1 +1,17 @@
+from enum import Enum
+from dataclasses import dataclass
+from typing import Optional
+from db_connect import get_connection
 
+
+class Role(Enum):
+    USER = "user"
+    ADMIN = "admin"
+
+@dataclass
+class User:
+    id: int
+    name: str
+    email: str
+    password: str
+    role: Role
