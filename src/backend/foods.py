2ebaedd776628e2fd7a6849d222a1aa949c71e58
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-11-09 13:52:30 +0100

    foods.py, create_food hozzáadása
    Added FoodManager class with create_food method for database entry.

--- src/backend/foods.py
+++ src/backend/foods.py
@@ -9,3 +9,18 @@ class Food:
     image: Optional[str]
     price: int
     category: Optional[str]
+
+class FoodManager:
+    
+#Új ételt hoz létre az adatbázisba
+#Meg kell adani neki bemenetbe:
+#a nevet, a leírást, a kép liknjét, az ár, a típus
+    def create_food(self, name: str, description: str, image: str, price: int, category: str):
+        conn = get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.callproc("create_food", [name, description, image, price, category])
+            conn.commit()
+        finally:
+            cursor.close()
+            conn.close()
