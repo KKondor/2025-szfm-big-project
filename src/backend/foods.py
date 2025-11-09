24085824ef4e7c77723e8bd6d4483277e1c5172b
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-11-09 12:38:30 +0100

    foods.py, class Food hozzáadása

--- src/backend/foods.py
+++ src/backend/foods.py
@@ -1,1 +1,10 @@
+from dataclasses import dataclass
+from typing import Optional
 
+@dataclass
+class Food:
+    id: int
+    name: str
+    description: Optional[str]
+    image: Optional[str]
+    price: int
