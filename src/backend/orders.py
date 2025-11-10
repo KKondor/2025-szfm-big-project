33be0ba50ef7feab56b3d88d44c7adb5bedd8613
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-11-10 13:21:25 +0100

    order.py, create_order hozzáadása

--- src/backend/orders.py
+++ src/backend/orders.py
@@ -23,3 +23,20 @@ class Order:
     note: Optional[str]
     total_price: int
     items: List[OrderItem]
+
+class OrderManager:
+
+#Új rendelést hoz létre az adatbázisba
+#Meg kell adani neki bemenetbe:
+#a felhasználó id, a megrendelt ételek id listában(egy étel többször is szerepelhet), a megjegyzést
+    def create_order(self, user_id: int, food_ids: list, note: str):
+        conn = get_connection()
+        cursor = conn.cursor()
+        try:
+            food_ids_json = json.dumps(food_ids)
+            cursor.callproc("create_order", [user_id, food_ids_json, note])
+            conn.commit()
+        finally:
+            cursor.close()
+            conn.close()
+
