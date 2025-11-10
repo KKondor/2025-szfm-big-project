5aa5b3ca7c7e546b0e0956b5b0f856eb9a2d8e93
Meka2002 <64187459+Meka2002@users.noreply.github.com>
2025-11-10 09:36:57 +0100

    orders.py, OrderItem és Order hozzáadása

--- src/backend/orders.py
+++ src/backend/orders.py
@@ -1,1 +1,23 @@
+from dataclasses import dataclass
+from typing import List, Optional
+from datetime import datetime
+from foods import Food
+from users import User
 
+@dataclass
+class OrderItem:
+    item_id: int
+    food: Food
+    quantity: int
+    item_price: int
+    
+
+@dataclass
+class Order:
+    order_id: int
+    user: User
+    order_date: datetime
+    status: str
+    note: Optional[str]
+    total_price: int
+    items: List[OrderItem]
